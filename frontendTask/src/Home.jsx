af72c74aa6b045d87caa15fb990f74df961a90bd
Aadi2510 <88594521+Aadi2510@users.noreply.github.com>
2024-06-20 19:04:39 +0530

    Update Home.jsx

--- frontendTask/src/Home.jsx
+++ frontendTask/src/Home.jsx
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
    <>
-   <div className='w-full h-[100vh] bg-gradient-to-r from-purple-600 to-blue-600 flex justify-center items-center flex-col gap-[50px]'>
-    <h1 className='text-white text-6xl font-bold'>WELCOME TO HANUMANT</h1>
+   <div className='w-full h-[100vh] bg-sky-500/75 flex justify-center items-center flex-col gap-[50px]'>
+    <h1 className='text-white text-6xl font-bold'>WELCOME TO DASHBOARD</h1>
     <div>
-        <Link to='/customer' className='p-4 bg-black text-white rounded-2xl text-2xl font-bold'>FOR CUSTOMER</Link>
-        <Link to='/product'  className='p-4 bg-black text-white rounded-2xl text-2xl font-bold ml-[20px]'>FOR PRODUCT</Link>
+        <Link to='/customer' className='p-4 bg-cyan-500 hover:bg-cyan-600 shadow-xl text-white rounded-2xl text-2xl font-bold'>FOR CUSTOMER</Link>
+        <Link to='/product'  className='p-4 bg-cyan-500 hover:bg-cyan-600 shadow-xl text-white rounded-2xl text-2xl font-bold ml-[20px]'>FOR PRODUCT</Link>
     </div>
    </div>
    </>
   )
 }
+
